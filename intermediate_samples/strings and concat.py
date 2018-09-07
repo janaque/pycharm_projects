bb4d912342ecb64908f95f422bd0005be6da6b1a
JULIUS.ANAQUE <janaque@gmail.com>
2018-09-07 14:33:41 +0800

    pycharm initial commit str and concat sample

--- intermediate_samples/strings and concat.py
+++ intermediate_samples/strings and concat.py
@@ -1,24 +1,14 @@
 
 import os
 
-var1 = input("Enter you first name: ")
-var2 = input("Enter you second name: ")
-var3 = input("Enter you third name: ")
-var4 = input("Enter you fourth name: ")
+location_of_files = 'C:\\Users\\julius.anaque\\PycharmProjects\\pycharmprojects'
+file_name = 'samplefile'
+
+#print(location_of_files + '\\' + file_name)
+
+with open(os.path.join(location_of_files, file_name)) as f:
+    print(f.read())
 
 
-names = [var1,
-         var2,
-         var3,
-         var4,
-         ]
 
-print('=================================')
-
-for var in names:
-    print('Hello there, ' + var)
-
-print('=================================')
-
-print(', '.join(names))
 
